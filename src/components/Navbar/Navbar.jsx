75c93e10811559f4627ec92ac771f38a4ef2183c
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-02 13:55:05 -0300

    Creating Navbar components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="nav false">
+            <img
+                className="nav__logo"
+                src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+                alt=""
+            />
+            <img
+                className="nav__avatar"
+                src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+                alt=""
+            />
+        </div>
+    )
+}
+
+export default Navbar
