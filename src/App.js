8435976fb1496e293377c918c86460d44b58df5d
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-03 20:43:28 -0300

    Using the new Row in App

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
-      <Row />
+      <Row title='Netflix Originals' movieType='netflix-originals' />
+      <Row title='Trending Now' movieType='trending-now' />
+      <Row title='Top Rated' movieType='top-rated' />
+      <Row title='Action Movies' movieType='action-movies' />
+      <Row title='Horror Movies' movieType='horror-movies' />
+      <Row title='Romance Movies' movieType='romance-movies' />
+      <Row title='Documentaries Movies' movieType='documentaries-movies' />
     </div>
   );
 }
