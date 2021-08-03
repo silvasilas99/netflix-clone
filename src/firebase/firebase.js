1c2e0ed913e52d905fd2ab3eec48ee1b8b0ab159
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-03 17:48:07 -0300

    First trial with Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+})
+
+const firebaseDatabase = app.database();
+
+export {
+    firebaseDatabase
+};
