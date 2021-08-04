f206b4c0b158c8efadd7ae5959531c64452e5367
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-03 23:28:03 -0300

    Switching App content to a page, and inserting React Route

---
+++ src/views/Home.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from "./../components/Navbar/Navbar";
+import Header from "./../components/Header/Header";
+import Row from "./../components/Row/Row";
+
+function Home() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Header />
+            <Row title='Netflix Originals' movieType='netflix-originals' />
+            <Row title='Trending Now' movieType='trending-now' />
+            <Row title='Top Rated' movieType='top-rated' />
+            <Row title='Action Movies' movieType='action-movies' />
+            <Row title='Horror Movies' movieType='horror-movies' />
+            <Row title='Romance Movies' movieType='romance-movies' />
+            <Row title='Documentaries Movies' movieType='documentaries-movies' />
+        </div>
+    )
+}
+
+export default Home
