57307838ce1289c4e6311438dc3adbede5fef1b0
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-03 23:49:04 -0300

    Using the login view in a path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+
 import Home from './views/Home';
+import Login from './views/Login';
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
