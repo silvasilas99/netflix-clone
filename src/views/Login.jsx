8d9aed549fa7448694851e26916879d7a48e9d30
Silas S. da Silva <silvasilas66@gmail.com>
2021-08-03 23:48:44 -0300

    Creating login view

---
+++ src/views/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar/Navbar"
+import LoginForm from "../components/LoginForm/LoginForm"
+
+function Login() {
+    return (
+        <div className="main">
+            <Navbar />
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Login
